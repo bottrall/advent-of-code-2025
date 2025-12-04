577607f665b78f8d4e562f445a80318d079f1f20
Jake Bottrall <jakebottrall@gmail.com>
2025-12-03 19:32:07 -0800

    add day three solution with part one and part two functions

---
+++ src/day-three.ts
@@ -0,0 +1,60 @@
+import fs from "node:fs";
+
+export function partOne(input: string) {
+  const numOfRequiredBatteries = 2;
+
+  let totalJoltage = 0;
+
+  input.split("\n").forEach((bank, n) => {
+    const batteries = bank.split("").map(Number);
+
+    let searchFromIndex = 0;
+    let joltage = "";
+
+    for (let i = numOfRequiredBatteries; i > 0; i--) {
+      const end = batteries.length + 1 - i;
+      const battery = Math.max(...batteries.slice(searchFromIndex, end));
+
+      joltage += String(battery);
+
+      searchFromIndex = batteries.indexOf(battery, searchFromIndex) + 1;
+    }
+
+    totalJoltage += Number(joltage);
+  });
+
+  return totalJoltage;
+}
+
+export function partTwo(input: string) {
+  const numOfRequiredBatteries = 12;
+
+  let totalJoltage = 0;
+
+  input.split("\n").forEach((bank, n) => {
+    const batteries = bank.split("").map(Number);
+
+    let searchFromIndex = 0;
+    let joltage = "";
+
+    for (let i = numOfRequiredBatteries; i > 0; i--) {
+      const end = batteries.length + 1 - i;
+      const battery = Math.max(...batteries.slice(searchFromIndex, end));
+
+      joltage += String(battery);
+
+      searchFromIndex = batteries.indexOf(battery, searchFromIndex) + 1;
+    }
+
+    totalJoltage += Number(joltage);
+  });
+
+  return totalJoltage;
+}
+
+if (import.meta.url === `file://${process.argv.at(1)}`) {
+  const input = fs.readFileSync("src/day-three.input.txt", "utf-8");
+
+  console.log(partOne(input));
+  console.log(partTwo(input));
+}
